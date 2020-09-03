ad1b99ca6648d631afeab17c14e84ffcabe26311
Jerry <huangjun0529@126.com>
2020-09-03 11:22:18 +0800

    我的页面完成

---
+++ U17Test/U17Test/Classes/Controller/Mine/View/mineCell.swift
@@ -0,0 +1,13 @@
+//
+//  mineCell.swift
+//  U17Test
+//
+//  Created by hjun on 2020/9/3.
+//  Copyright © 2020 hjun. All rights reserved.
+//
+
+import UIKit
+
+class mineCell: UICollectionViewCell {
+    
+}
---
+++ U17Test/U17Test/Classes/Controller/Mine/View/mineHeadView.swift
@@ -0,0 +1,21 @@
+//
+//  mineHeadView.swift
+//  U17Test
+//
+//  Created by hjun on 2020/9/3.
+//  Copyright © 2020 hjun. All rights reserved.
+//
+
+import UIKit
+
+class mineHeadView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
